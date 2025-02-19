499d7111b29f500c8cebc3a453d698e01c9e28fd
ilchan123 <dlfcks9@gmail.com>
2025-02-19 22:37:30 +0900

    feat: dotenv 설치 및 .env 적용

--- front/thanos_call/src/services/firebase.js
+++ front/thanos_call/src/services/firebase.js
@@ -8,12 +8,12 @@ import {
 } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCF3kR7EqOBTc7145gsjJMQyoFj3Knwyjk",
-  authDomain: "thanoscall-30729.firebaseapp.com",
-  projectId: "thanoscall-30729",
-  storageBucket: "thanoscall-30729.firebasestorage.app",
-  messagingSenderId: "422555274700",
-  appId: "1:422555274700:web:726ffdd4b3f1fdf0242169",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 }
 
 const app = initializeApp(firebaseConfig)
