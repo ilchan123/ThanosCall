2bafc3b0c073df4581e2c4b31683dde75a589436
ilchan123 <dlfcks9@gmail.com>
2025-02-26 02:42:32 +0900

    fix: 경로 설정(데이타 분석)

--- front/thanos_call/src/utils/layout.jsx
+++ front/thanos_call/src/utils/layout.jsx
@@ -3,11 +3,15 @@ import { Routes, Route } from "react-router-dom"
 import LoginPage from "../pages/login"
 import CallCenterPage from "../pages/call_center"
 import { UserProvider } from "../context/UserContext"
-import UserDataStaticsChart from "../components/charts/UserDataStaticsChart"
-import PrdDataStaticsChart from "../components/charts/PrdDataStaticsChart"
-import CallInfoStaticsChart from "../components/charts/CallInfoStaticsChart"
-import CSStaticsChart from "../components/charts/CSStaticsChart"
-import DataFrame from "../components/cs_list_manage/data_frame"
+import UserDataStaticsChart from "../components/cs_data_analysis/charts/UserDataStaticsChart"
+import PrdDataStaticsChart from "../components/cs_data_analysis/charts/PrdDataStaticsChart"
+import CallInfoStaticsChart from "../components/cs_data_analysis/charts/CallInfoStaticsChart"
+import CSStaticsChart from "../components/cs_data_analysis/charts/CSStaticsChart"
+import All from "../components/cs_data_analysis/chart_view/all"
+import CallLog from "../components/cs_data_analysis/chart_view/call_log"
+import MemberInfo from "../components/cs_data_analysis/chart_view/member_info"
+import OrderLog from "../components/cs_data_analysis/chart_view/order_log"
+import ProductLog from "../components/cs_data_analysis/chart_view/product_log"
 
 const Layout = () => {
   return (
@@ -19,11 +23,11 @@ const Layout = () => {
           <Route path="data/product_ds" element={<PrdDataStaticsChart />} />
           <Route path="data/callInfo_ds" element={<CallInfoStaticsChart />} />
           <Route path="data/cs_statics" element={<CSStaticsChart />} />
-          <Route path="data/all" element={<DataFrame />} />
-          <Route path="data/order_log" element={<DataFrame />} />
-          <Route path="data/member_info" element={<DataFrame />} />
-          <Route path="data/currency_log" element={<DataFrame />} />
-          <Route path="data/product_log" element={<DataFrame />} />
+          <Route path="data/all" element={<All />} />
+          <Route path="data/order_log" element={<OrderLog />} />
+          <Route path="data/member_info" element={<MemberInfo />} />
+          <Route path="data/call_log" element={<CallLog />} />
+          <Route path="data/product_log" element={<ProductLog />} />
         </Route>
       </Routes>
     </UserProvider>
